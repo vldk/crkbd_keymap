b03aeae3148d1a3ab21008485e5d26b4f1af37e5
Volodymyr Kravchuk <volodymyr.kravchuk@onseo.biz>
2023-10-15 20:39:31 +0300

    Updated doc, layout changes

--- keymap.c
+++ keymap.c
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifdef COMBO_ENABLE
 
 enum combos {
-  TABQ_ESC
+  TABQ_ESC,
 };
 const uint16_t PROGMEM tab_q[] = {KC_TAB, KC_Q, COMBO_END};
 // const uint16_t PROGMEM test_combo2[] = {KC_C, KC_D, COMBO_END};
@@ -37,7 +37,8 @@ combo_t key_combos[] = {
 
 
 enum {
-    TD_MEDIA
+    TD_MEDIA,
+    TD_VOL,
 };
 
 void dance_media(tap_dance_state_t *state, void *user_data) {
@@ -52,10 +53,21 @@ void dance_media(tap_dance_state_t *state, void *user_data) {
     }
 }
 
+void dance_vol(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1)  {
+        tap_code(KC_VOLD);
+    } else if (state->count == 2) {
+        tap_code(KC_MUTE);
+    } else {
+        reset_tap_dance(state);
+    }
+}
+
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // [TD_MEDIA] =  ACTION_TAP_DANCE_DOUBLE(KC_MPLY, KC_MNXT)
-    [TD_MEDIA] =  ACTION_TAP_DANCE_FN(dance_media)
+    [TD_MEDIA] =  ACTION_TAP_DANCE_FN(dance_media),
+    [TD_VOL] = ACTION_TAP_DANCE_FN(dance_vol)
 };
 
 
@@ -66,28 +78,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                              KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                             KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                             KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                                KC_RALT, MO(_LOWER), KC_SPC,            KC_ENT, MO(_RAISE), MO(_ADJUST)
+                                KC_RALT, MO(_LOWER), KC_SPC,       KC_ENT, MO(_RAISE), KC_RGUI
     ),
 	[_LOWER] = LAYOUT_split_3x6_3(
-        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, C(RCTL(KC_BSPC)),
-        KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_GRV,
-        KC_TRNS, KC_BRID, KC_BRIU, KC_VOLD, TD(TD_MEDIA), KC_VOLU,               KC_LBRC, KC_RBRC, KC_EQL, KC_MINS, KC_BSLS, KC_TILD,
-                                        KC_TRNS, KC_TRNS, KC_LCTL,  KC_TRNS, KC_TRNS, KC_RGUI
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5,                                  KC_6, KC_7, KC_8, KC_9, KC_0, C(RCTL(KC_BSPC)),
+        KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_GRV,
+        KC_TRNS, KC_BRID, KC_BRIU, TD(TD_VOL), TD(TD_MEDIA), KC_VOLU,            KC_LBRC, KC_RBRC, KC_EQL, KC_MINS, KC_BSLS, KC_TILD,
+                                        KC_TRNS, KC_TRNS , KC_TRNS,      KC_TRNS, KC_TRNS, KC_APP
     ),
 	[_RAISE] = LAYOUT_split_3x6_3(
         QK_GESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                         KC_PSCR, C(RCTL(KC_LEFT)), KC_UP, C(RCTL(KC_RGHT)), KC_PGUP, C(RCTL(KC_BSPC)),
         KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                        KC_INS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_HOME,
         KC_TRNS, KC_F11, KC_F12, KC_NO, KC_NO, KC_CAPS,                     KC_DEL, KC_NO, KC_PPLS, KC_PMNS, KC_SLSH, KC_END,
-                                        KC_TRNS, KC_TRNS, KC_LCTL,  KC_TRNS, KC_TRNS, KC_APP
+                                        KC_TRNS, KC_TRNS, KC_RGUI,    KC_TRNS, KC_NO, KC_APP
     ),
 	[_ADJUST] = LAYOUT_split_3x6_3(
-        RGB_RST, RGB_ATG, KC_NO, KC_SCRL, KC_PAUS, KC_NUM,                     KC_CALC, KC_P7, KC_P8, KC_P9, KC_PAST, KC_PPLS,
-        EH_RGHT, RGB_PST, RGB_UND, RGB_SAI, RGB_HUI, RGB_TOG,               KC_PDOT, KC_P4, KC_P5, KC_P6, KC_PSLS, KC_PMNS,
-        EH_LEFT, RGB_PCT, RGB_IDL, RGB_VAI, RGB_SPI, RGB_MOD,               KC_P0,   KC_P1, KC_P2, KC_P3, KC_NO, KC_PENT,
-                                        KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS
+        RGB_RST, RGB_ATG, KC_NO, KC_SCRL, KC_PAUS, KC_NUM,                      KC_CALC, KC_P7, KC_P8, KC_P9, KC_PAST, KC_PPLS,
+        EH_RGHT, RGB_PST, RGB_UND, RGB_SAI, RGB_HUI, RGB_TOG,                   KC_PDOT, KC_P4, KC_P5, KC_P6, KC_PSLS, KC_PMNS,
+        EH_LEFT, RGB_PCT, RGB_IDL, RGB_VAI, RGB_SPI, RGB_MOD,                   KC_P0,   KC_P1, KC_P2, KC_P3, KC_NO, KC_PENT,
+                                        KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    state = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
+    return state;
+}
 
 
 void suspend_power_down_keymap(void) {
--- readme.md
+++ readme.md
@@ -1,9 +1,53 @@
+## Keymap
 
+```c
 
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[_QWERTY] = LAYOUT_split_3x6_3(
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                              KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                             KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                             KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                                KC_RALT, MO(_LOWER), KC_SPC,       KC_ENT, MO(_RAISE), KC_RGUI
+    ),
+	[_LOWER] = LAYOUT_split_3x6_3(
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5,                                  KC_6, KC_7, KC_8, KC_9, KC_0, C(RCTL(KC_BSPC)),
+        KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_GRV,
+        KC_TRNS, KC_BRID, KC_BRIU, TD(TD_VOL), TD(TD_MEDIA), KC_VOLU,            KC_LBRC, KC_RBRC, KC_EQL, KC_MINS, KC_BSLS, KC_TILD,
+                                        KC_TRNS, KC_TRNS , KC_TRNS,      KC_TRNS, KC_TRNS, KC_APP
+    ),
+	[_RAISE] = LAYOUT_split_3x6_3(
+        QK_GESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                         KC_PSCR, C(RCTL(KC_LEFT)), KC_UP, C(RCTL(KC_RGHT)), KC_PGUP, C(RCTL(KC_BSPC)),
+        KC_TRNS, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                        KC_INS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_HOME,
+        KC_TRNS, KC_F11, KC_F12, KC_NO, KC_NO, KC_CAPS,                     KC_DEL, KC_NO, KC_PPLS, KC_PMNS, KC_SLSH, KC_END,
+                                        KC_TRNS, KC_TRNS, KC_RGUI,    KC_TRNS, KC_NO, KC_APP
+    ),
+	[_ADJUST] = LAYOUT_split_3x6_3(
+        RGB_RST, RGB_ATG, KC_NO, KC_SCRL, KC_PAUS, KC_NUM,                      KC_CALC, KC_P7, KC_P8, KC_P9, KC_PAST, KC_PPLS,
+        EH_RGHT, RGB_PST, RGB_UND, RGB_SAI, RGB_HUI, RGB_TOG,                   KC_PDOT, KC_P4, KC_P5, KC_P6, KC_PSLS, KC_PMNS,
+        EH_LEFT, RGB_PCT, RGB_IDL, RGB_VAI, RGB_SPI, RGB_MOD,                   KC_P0,   KC_P1, KC_P2, KC_P3, KC_NO, KC_PENT,
+                                        KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS, KC_TRNS, KC_TRNS
+    )
+};
+```
+
+## Tab-Dacnce
+
+**TD_MEDIA**
+1 tap -  MEDIA_PLAY_PAUSE
+2 taps - MEDIA_NEXT_TRACK
+3 taps - MEDIA_PREV_TRACK
+
+**TD_VOL**
+1 tap -  AUDIO_VOL_DOWN
+2 taps - AUDIO_MUTE
+
+## Combo
+[_LOWER + _RAISE] => _ADJUST 
+[TAB + Q] => ESC
 
 ## RGB
 
-I took a big inspiration from Drashna's RGB configuration and tweaked it.
+RGB implementation is based at `keyboards/crkbd/keymaps/rpbaptist``
 
 - `RGB_UND`: Toggles underglow indicators on and off. Each layer has its own color.
 - `RGB_IDL`: This will enable/disable idle mode. By default an "active" RGB mode will be used. When the idle timeout has exceeded the secondary animation will be activated, which is a passive animation. When switching to `GAMING` layer the timeout will be reduced and the idle animation changed to `RGB_MATRIX_RAINBOW_PINWHEELS`.
@@ -14,8 +58,6 @@ I took a big inspiration from Drashna's RGB configuration and tweaked it.
 - `RGB_PCT`: Cycles through two, more colorful, passive modes: `RGB_MATRIX_RAINBOW_PINWHEELS` -> `RGB_MATRIX_CYCLE_LEFT_RIGHT`
 
 ## Building
-
-`qmk flash -bl dfu -kb crkbd -km vldk`
 
  `qmk flash -kb crkbd -km vldk -bl avrdude-split-left`
  `qmk flash -kb crkbd -km vldk -bl avrdude-split-right`
